9000cef24645f86da4ab8d2a59303d712da4ffd6
PratibhaMastud <karandepratibha999@gmail.com>
2020-11-22 23:47:56 +0530

    Added:WAP to start the game.

--- SnakeAndLadderGame/src/Game/SnakeLadderGame.java
+++ SnakeAndLadderGame/src/Game/SnakeLadderGame.java
@@ -2,8 +2,14 @@ package Game;
 
 public class SnakeLadderGame {
 
+    static int startPosition=0;
+	static int  topPosition=0;
+	
 	public static void main(String[] args) {
-		System.out.println("Welcome to Snake and ladder game");
+		System.out.println("----------------------Welcome to Snake and ladder game----------------------");
+		System.out.println("The game Played with single Player at the start position is : "+startPosition);
+		System.out.println("----------------------------------------------------------------------------");
+		
 
 	}
 
