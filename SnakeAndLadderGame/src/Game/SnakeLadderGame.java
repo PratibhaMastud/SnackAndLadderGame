99f795519ce82f2547a5607dd69dac8121a4d975
PratibhaMastud <karandepratibha999@gmail.com>
2020-11-23 00:21:29 +0530

    Added:write a program to Play game by Rolls Die

--- SnakeAndLadderGame/src/Game/SnakeLadderGame.java
+++ SnakeAndLadderGame/src/Game/SnakeLadderGame.java
@@ -1,16 +1,25 @@
 package Game;
 
+import java.util.Random;
+
 public class SnakeLadderGame {
 
     static int startPosition=0;
-	static int  topPosition=0;
+	static int topPosition=0;
+	static int currentPosition=0;
+	
+	public static int findNumRollsDie() {
+		
+		Random random = new Random();
+		int rollsDieNumber = random.nextInt(6)+1;
+		return rollsDieNumber;
+	}
 	
 	public static void main(String[] args) {
-		System.out.println("----------------------Welcome to Snake and ladder game----------------------");
-		System.out.println("The game Played with single Player at the start position is : "+startPosition);
-		System.out.println("----------------------------------------------------------------------------");
-		
 
+		currentPosition += findNumRollsDie();
+		System.out.println("The Player Current Position is : "+currentPosition);
+	
 	}
 
 }
