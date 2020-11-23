fce6ebdb3f3ad4066475e9a70d0844434ffad763
PratibhaMastud <karandepratibha999@gmail.com>
2020-11-23 09:34:50 +0530

    Added:Check condition of the Player

--- SnakeAndLadderGame/src/Game/SnakeLadderGame.java
+++ SnakeAndLadderGame/src/Game/SnakeLadderGame.java
@@ -3,22 +3,46 @@ package Game;
 import java.util.Random;
 
 public class SnakeLadderGame {
-
-    static int startPosition=0;
-	static int currentPosition=0;
 	
-	public static int findNumRollsDie() {
-		
-		Random random = new Random();
-		int rollsDieNumber = random.nextInt(6)+1;
-		return rollsDieNumber;
+	static int currentPosition=0;
+	static int noPlay=0;
+	
+	public static int playRollDie() {
+		Random random=new Random();			
+		int rollNumber = random.nextInt(6)+1;
+		return rollNumber;
+	}
+	public static int playLadder() {
+		Random random=new Random();			
+		int ladderStep = random.nextInt(6)+1;
+		System.out.println(ladderStep);
+		return ladderStep;
+	}
+	public static int snackAttack() {
+		Random random=new Random();			
+		int snackBite = random.nextInt(6)+1;
+		return snackBite;
 	}
 	
 	public static void main(String[] args) {
+		
+		int dieNumber=playRollDie();
 
-		currentPosition += findNumRollsDie();
-		System.out.println("The Player Current Position is : "+currentPosition);
-	
+		if(dieNumber==0) {
+			currentPosition = noPlay;
+			System.out.println("No play");
+
+			
+		}else if(dieNumber==1) {
+				System.out.println("Got ladder");
+				currentPosition = currentPosition + playLadder();
+				
+		}else if(dieNumber==2) {
+				System.out.println("Snake attack");
+				currentPosition = currentPosition - snackAttack();	
+
+			}
+
 	}
 
 }
